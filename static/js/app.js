958adfab3cad462abd91df2ebe774c5dd029c39b
Yuriy <yuriy.lomakin@gmail.com>
2016-08-06 18:22:03 +0300

    edit login modal

--- static/js/app.js
+++ static/js/app.js
@@ -963,51 +963,6 @@
 
 })(window, document, window.jQuery);
 
-// CLASSYLOADER
-// ----------------------------------- 
-
-(function(window, document, $, undefined){
-
-  $(function(){
-
-    var $scroller       = $(window),
-        inViewFlagClass = 'js-is-in-view'; // a classname to detect when a chart has been triggered after scroll
-
-    $('[data-classyloader]').each(initClassyLoader);
-    
-    function initClassyLoader() {
-    
-      var $element = $(this),
-          options  = $element.data();
-      
-      // At lease we need a data-percentage attribute
-      if(options) {
-        if( options.triggerInView ) {
-
-          $scroller.scroll(function() {
-            checkLoaderInVIew($element, options);
-          });
-          // if the element starts already in view
-          checkLoaderInVIew($element, options);
-        }
-        else
-          startLoader($element, options);
-      }
-    }
-    function checkLoaderInVIew(element, options) {
-      var offset = -20;
-      if( ! element.hasClass(inViewFlagClass) &&
-          $.Utils.isInView(element, {topoffset: offset}) ) {
-        startLoader(element, options);
-      }
-    }
-    function startLoader(element, options) {
-      element.ClassyLoader(options).addClass(inViewFlagClass);
-    }
-
-  });
-
-})(window, document, window.jQuery);
 
 /**=========================================================
  * Module: clear-storage.js
