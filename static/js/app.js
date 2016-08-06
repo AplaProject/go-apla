db85ab775151965af59d190925896e86385b34ab
Yuriy <yuriy.lomakin@gmail.com>
2016-08-06 17:17:45 +0300

    modal

--- static/js/app.js
+++ static/js/app.js
@@ -2670,35 +2670,6 @@
 
 })(window, document, window.jQuery);
 
-// SPARKLINE
-// ----------------------------------- 
-
-(function(window, document, $, undefined){
-
-  $(function(){
-
-    $('[data-sparkline]').each(initSparkLine);
-
-    function initSparkLine() {
-      var $element = $(this),
-          options = $element.data(),
-          values  = options.values && options.values.split(',');
-
-      options.type = options.type || 'bar'; // default chart is bar
-      options.disableHiddenCheck = true;
-
-      $element.sparkline(values, options);
-
-      if(options.resize) {
-        $(window).resize(function(){
-          $element.sparkline(values, options);
-        });
-      }
-    }
-  });
-
-})(window, document, window.jQuery);
-
 // Sweet Alert
 // ----------------------------------- 
 
