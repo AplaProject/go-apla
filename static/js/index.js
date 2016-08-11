3ac53f03f7364b9892883e2f658288689e1de11f
Yuriy <yuriy.lomakin@gmail.com>
2016-08-11 11:36:19 +0300

    add preloader()

--- static/js/index.js
+++ static/js/index.js
@@ -33,6 +33,14 @@ function Alert(title, text, type) {
 	});
 }
 
+function preloader() {
+	var obj = $("[data-sweet-alert]");
+	obj.each(function() {
+		var el = $(this);
+		el.append('<div class="sk-cube-grid"><div class="sk-cube sk-cube1"></div><div class="sk-cube sk-cube2"></div><div class="sk-cube sk-cube3"></div><div class="sk-cube sk-cube4"></div><div class="sk-cube sk-cube5"></div><div class="sk-cube sk-cube6"></div><div class="sk-cube sk-cube7"></div><div class="sk-cube sk-cube8"></div><div class="sk-cube sk-cube9"></div></div>');
+	});
+}
+
 function dl_navigate (page, parameters) {
     var json = JSON.stringify(parameters);
     //$('#loader').spin();
