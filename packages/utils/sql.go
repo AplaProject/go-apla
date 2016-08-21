06bd5d28fc06f6106f2a0ae113268e5af59554b4
c-darwin <darwin84@yandex.ru>
2016-08-21 12:22:45 +0300

    added debug

--- packages/utils/sql.go
+++ packages/utils/sql.go
@@ -1078,6 +1078,7 @@ func (db *DCDB) DbLock(DaemonCh chan bool, AnswerDaemonCh chan string, goRoutine
 		}
 	}()
 
+	log.Debug("DbLock")
 	var ok bool
 	for {
 		select {
@@ -1089,6 +1090,8 @@ func (db *DCDB) DbLock(DaemonCh chan bool, AnswerDaemonCh chan string, goRoutine
 		}
 
 		Mutex.Lock()
+
+		log.Debug("DbLock Mutex.Lock()")
 
 		exists, err := db.OneRow("SELECT lock_time, script_name FROM main_lock").String()
 		if err != nil {
