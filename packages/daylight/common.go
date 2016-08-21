51fdb7ac8772a7e643f7845a0478e914fd30fe10
c-darwin <darwin84@yandex.ru>
2016-08-21 20:54:03 +0300

    refactoring

---
+++ packages/daylight/common.go
@@ -0,0 +1,103 @@
+package daylight
+
+import (
+	"fmt"
+	"github.com/astaxie/beego/session"
+	"github.com/DayLightProject/go-daylight/packages/consts"
+	"github.com/DayLightProject/go-daylight/packages/utils"
+	"github.com/op/go-logging"
+	_ "image/png"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"encoding/hex"
+)
+
+var (
+	log            = logging.MustGetLogger("dcoin")
+	format         = logging.MustStringFormatter("%{color}%{time:15:04:05.000} %{shortfile} %{shortfunc} [%{level:.4s}] %{color:reset} %{message}[" + consts.VERSION + "]" + string(byte(0)))
+	configIni      map[string]string
+	globalSessions *session.Manager
+)
+
+func firstBlock() {
+	if *utils.GenerateFirstBlock == 1 {
+		PublicKey, _ := ioutil.ReadFile(*utils.Dir + "/PublicKey")
+//		PublicKeyBytes, _ := base64.StdEncoding.DecodeString(string(PublicKey))
+		PublicKeyBytes,_ := hex.DecodeString(string(PublicKey))
+
+		NodePublicKey, _ := ioutil.ReadFile(*utils.Dir + "/NodePublicKey")
+//		NodePublicKeyBytes, _ := base64.StdEncoding.DecodeString(string(NodePublicKey))
+		NodePublicKeyBytes,_ := hex.DecodeString(string(NodePublicKey))
+		Host, _ := ioutil.ReadFile(*utils.Dir + "/Host")
+
+		tx := utils.DecToBin(1, 1)
+		tx = append(tx, utils.DecToBin(utils.Time(), 4)...)
+		tx = append(tx, utils.EncodeLengthPlusData("1")...) // wallet_id
+		tx = append(tx, utils.EncodeLengthPlusData("0")...) // citizen_id
+		tx = append(tx, utils.EncodeLengthPlusData(PublicKeyBytes)...)
+		tx = append(tx, utils.EncodeLengthPlusData(NodePublicKeyBytes)...)
+		tx = append(tx, utils.EncodeLengthPlusData(Host)...)
+
+		block := utils.DecToBin(0, 1)
+		block = append(block, utils.DecToBin(1, 4)...)
+		block = append(block, utils.DecToBin(utils.Time(), 4)...)
+		block = append(block, utils.EncodeLengthPlusData("1")...) // wallet_id
+		block = append(block, utils.DecToBin(0, 1)...) // cb_id
+		block = append(block, utils.EncodeLengthPlusData(tx)...)
+
+		static := filepath.Join("", "static")
+		if _, err := os.Stat(static); os.IsNotExist(err) {
+			if err = os.Mkdir(static, 0755); err != nil {
+				log.Error("%v", utils.ErrInfo(err))
+			} 
+		}
+		ioutil.WriteFile( filepath.Join( static, "1block"), block, 0644)
+		os.Exit(0)
+	}
+
+}
+
+// http://grokbase.com/t/gg/golang-nuts/12a9yhgr64/go-nuts-disable-directory-listing-with-http-fileserver#201210093cnylxyosmdfuf3wh5xqnwiut4
+func noDirListing(h http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasSuffix(r.URL.Path, "/") {
+			http.NotFound(w, r)
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
+func openBrowser(BrowserHttpHost string) {
+	log.Debug("runtime.GOOS: %v", runtime.GOOS)
+	var err error
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", BrowserHttpHost).Start()
+	case "windows", "darwin":
+		err = exec.Command("open", BrowserHttpHost).Start()
+		if err != nil {
+			exec.Command("cmd", "/c", "start", BrowserHttpHost).Start()
+		}
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		log.Error("%v", err)
+	}
+}
+
+func GetHttpHost() (string, string, string) {
+	BrowserHttpHost := "http://localhost:"+*utils.ListenHttpPort
+	HandleHttpHost := ""
+	ListenHttpHost := ":"+*utils.ListenHttpPort
+	if len(*utils.TcpHost) > 0 {
+		ListenHttpHost = *utils.TcpHost+":"+*utils.ListenHttpPort
+	}
+	return BrowserHttpHost, HandleHttpHost, ListenHttpHost
+}
--- packages/daylight/daylight_start.go
+++ packages/daylight/daylight_start.go
@@ -1,10 +1,9 @@
-package dcoin
+package daylight
 
 import (
 	"encoding/json"
 	"fmt"
 	"github.com/astaxie/beego/config"
-	"github.com/astaxie/beego/session"
 	"github.com/DayLightProject/go-daylight/packages/consts"
 	"github.com/DayLightProject/go-daylight/packages/controllers"
 	"github.com/DayLightProject/go-daylight/packages/daemons"
@@ -27,80 +26,10 @@ import (
 	"os/exec"
 	"path/filepath"
 	"runtime"
-	"strings"
 	"time"
-//	"encoding/base64"
-	"encoding/hex"
 )
 
-var (
-	log            = logging.MustGetLogger("dcoin")
-	format         = logging.MustStringFormatter("%{color}%{time:15:04:05.000} %{shortfile} %{shortfunc} [%{level:.4s}] %{color:reset} %{message}[" + consts.VERSION + "]" + string(byte(0)))
-	configIni      map[string]string
-	globalSessions *session.Manager
-)
 
-func Stop() {
-	log.Debug("Stop()")
-	IosLog("Stop()")
-	var err error
-	utils.DB, err = utils.NewDbConnect(configIni)
-	log.Debug("DCOIN Stop : %v", utils.DB)
-	IosLog("utils.DB:" + fmt.Sprintf("%v", utils.DB))
-	if err != nil {
-		IosLog("err:" + fmt.Sprintf("%s", utils.ErrInfo(err)))
-		log.Error("%v", utils.ErrInfo(err))
-		//panic(err)
-		//os.Exit(1)
-	}
-	err = utils.DB.ExecSql(`INSERT INTO stop_daemons(stop_time) VALUES (?)`, utils.Time())
-	if err != nil {
-		IosLog("err:" + fmt.Sprintf("%s", utils.ErrInfo(err)))
-		log.Error("%v", utils.ErrInfo(err))
-	}
-	log.Debug("DCOIN Stop")
-	IosLog("DCOIN Stop")
-}
-
-func firstBlock() {
-	if *utils.GenerateFirstBlock == 1 {
-
-
-		PublicKey, _ := ioutil.ReadFile(*utils.Dir + "/PublicKey")
-//		PublicKeyBytes, _ := base64.StdEncoding.DecodeString(string(PublicKey))
-		PublicKeyBytes,_ := hex.DecodeString(string(PublicKey))
-
-		NodePublicKey, _ := ioutil.ReadFile(*utils.Dir + "/NodePublicKey")
-//		NodePublicKeyBytes, _ := base64.StdEncoding.DecodeString(string(NodePublicKey))
-		NodePublicKeyBytes,_ := hex.DecodeString(string(NodePublicKey))
-		Host, _ := ioutil.ReadFile(*utils.Dir + "/Host")
-
-		tx := utils.DecToBin(1, 1)
-		tx = append(tx, utils.DecToBin(utils.Time(), 4)...)
-		tx = append(tx, utils.EncodeLengthPlusData("1")...) // wallet_id
-		tx = append(tx, utils.EncodeLengthPlusData("0")...) // citizen_id
-		tx = append(tx, utils.EncodeLengthPlusData(PublicKeyBytes)...)
-		tx = append(tx, utils.EncodeLengthPlusData(NodePublicKeyBytes)...)
-		tx = append(tx, utils.EncodeLengthPlusData(Host)...)
-
-		block := utils.DecToBin(0, 1)
-		block = append(block, utils.DecToBin(1, 4)...)
-		block = append(block, utils.DecToBin(utils.Time(), 4)...)
-		block = append(block, utils.EncodeLengthPlusData("1")...) // wallet_id
-		block = append(block, utils.DecToBin(0, 1)...) // cb_id
-		block = append(block, utils.EncodeLengthPlusData(tx)...)
-
-		static := filepath.Join("", "static")
-		if _, err := os.Stat(static); os.IsNotExist(err) {
-			if err = os.Mkdir(static, 0755); err != nil {
-				log.Error("%v", utils.ErrInfo(err))
-			} 
-		}
-		ioutil.WriteFile( filepath.Join( static, "1block"), block, 0644)
-		os.Exit(0)
-	}
-
-}
 
 func Start(dir string, thrustWindowLoder *window.Window) {
 
@@ -267,7 +196,7 @@ func Start(dir string, thrustWindowLoder *window.Window) {
 			if err != nil {
 				log.Error("%v", utils.ErrInfo(err))
 			}
-	
+
 			if thrustWindowLoder != nil {
 				thrustWindowLoder.Close()
 			}
@@ -420,9 +349,9 @@ func Start(dir string, thrustWindowLoder *window.Window) {
 				thrustWindow.HandleRemote(func(er commands.EventResult, this *window.Window) {
 					fmt.Println("RemoteMessage Recieved:", er.Message.Payload)
 					if len(er.Message.Payload) > 7 && er.Message.Payload[:7] == `mailto:` && runtime.GOOS == `windows` {
-						utils.ShellExecute(er.Message.Payload)	
+						utils.ShellExecute(er.Message.Payload)
 					} else if len(er.Message.Payload) >= 7 && er.Message.Payload[:7]==`USERID=` {
-						// for Lite version - do nothing 
+						// for Lite version - do nothing
 					} else {
 						openBrowser(er.Message.Payload)
 					}
@@ -448,44 +377,4 @@ func Start(dir string, thrustWindowLoder *window.Window) {
 	fmt.Println("ALL RIGHT")
 	utils.Sleep(3600 * 24 * 90)
 	log.Debug("EXIT")
-}
-
-// http://grokbase.com/t/gg/golang-nuts/12a9yhgr64/go-nuts-disable-directory-listing-with-http-fileserver#201210093cnylxyosmdfuf3wh5xqnwiut4
-func noDirListing(h http.Handler) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if strings.HasSuffix(r.URL.Path, "/") {
-			http.NotFound(w, r)
-			return
-		}
-		h.ServeHTTP(w, r)
-	})
-}
-
-func openBrowser(BrowserHttpHost string) {
-	log.Debug("runtime.GOOS: %v", runtime.GOOS)
-	var err error
-	switch runtime.GOOS {
-	case "linux":
-		err = exec.Command("xdg-open", BrowserHttpHost).Start()
-	case "windows", "darwin":
-		err = exec.Command("open", BrowserHttpHost).Start()
-		if err != nil {
-			exec.Command("cmd", "/c", "start", BrowserHttpHost).Start()
-		}
-	default:
-		err = fmt.Errorf("unsupported platform")
-	}
-	if err != nil {
-		log.Error("%v", err)
-	}
-}
-
-func GetHttpHost() (string, string, string) {
-	BrowserHttpHost := "http://localhost:"+*utils.ListenHttpPort
-	HandleHttpHost := ""
-	ListenHttpHost := ":"+*utils.ListenHttpPort
-	if len(*utils.TcpHost) > 0 {
-		ListenHttpHost = *utils.TcpHost+":"+*utils.ListenHttpPort
-	}
-	return BrowserHttpHost, HandleHttpHost, ListenHttpHost
 }
