3553c325351fcddba3ec895fb380768880b4690b
c-darwin <darwin84@yandex.ru>
2016-08-21 11:24:34 +0300

    upd tcp host/port

--- packages/utils/utils.go
+++ packages/utils/utils.go
@@ -76,7 +76,7 @@ type DaemonsChansType struct {
 }
 var (
 	TcpHost = flag.String("tcpHost", "", "tcpHost (e.g. 127.0.0.1)")
-	ListenHttpPort = flag.String("listenHttpPort", "8089", "ListenHttpPort")
+	ListenHttpPort = flag.String("listenHttpPort", "7079", "ListenHttpPort")
 	GenerateFirstBlock = flag.Int64("generateFirstBlock", 0, "generateFirstBlock")
 	OldVersion = flag.String("oldVersion", "", "")
 	TestRollBack = flag.Int64("testRollBack", 0, "testRollBack")
