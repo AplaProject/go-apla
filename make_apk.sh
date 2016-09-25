1ff05999878a2ae7398546a93a4d553bc65e4eee
c-darwin <darwin84@yandex.ru>
2016-09-25 20:13:54 +0300

    refactoring

--- make_apk.sh
+++
@@ -1,1 +0,0 @@
-./bindata.sh && ./make_res.sh && ANDROID_HOME=/home/z/android-sdk-linux go generate github.com/c-darwin/mobile/cmd/gomobile/ && go install github.com/c-darwin/mobile/cmd/gomobile/ && CGO_ENABLED=1 GOOS=android ANDROID_HOME=/home/z/android-sdk-linux gomobile build -v github.com/democratic-coin/dcoin-go ; rm -rf R.jar
