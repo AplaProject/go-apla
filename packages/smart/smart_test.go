49133870b3dfc9b488d5e792145f22d658fae486
Alexey Krivonogov <akrivonogov@gmail.com>
2016-09-27 09:17:13 +0500

    Added smart package

---
+++ packages/smart/smart_test.go
@@ -0,0 +1,46 @@
+// Copyright 2016 The go-daylight Authors
+// This file is part of the go-daylight library.
+//
+// The go-daylight library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-daylight library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-daylight library. If not, see <http://www.gnu.org/licenses/>.
+
+package smart
+
+import (
+	//	"fmt"
+	"testing"
+)
+
+type TestSmart struct {
+	Input  string
+	Output string
+}
+
+func TestNewContract(t *testing.T) {
+	test := []TestSmart{
+		{`contract my {
+			func init {
+			}
+			func front {
+			}
+			func main {
+			}
+}			
+		`, ``},
+	}
+	for _, item := range test {
+		if err := NewContract(item.Input); err != nil {
+			t.Error(err)
+		}
+	}
+}
