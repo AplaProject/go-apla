df361e1a83e1f5eb0e8ba83dacb6cbc4b6943338
Yuriy <yuriy.lomakin@gmail.com>
2016-12-02 09:15:28 +0200

    add macfix

--- static/js/index.js
+++ static/js/index.js
@@ -776,3 +776,11 @@ $(document).on('keydown', function (e) {
 		}
 	}
 });
+
+jQuery.os = { name: (/(win|mac|linux|sunos|solaris|iphone|ipad)/.exec(navigator.platform.toLowerCase()) || [u])[0].replace('sunos', 'solaris') };
+if (jQuery.os.name === "mac" || jQuery.os.name === "iphone" || jQuery.os.name === "ipad") {
+	$("body").addClass("macfix");
+}
+if (jQuery.os.name === "linux") {
+	$("body").addClass("androidfix");
+}
