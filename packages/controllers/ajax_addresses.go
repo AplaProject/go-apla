4dae8ce1f84b394885920b3f5cec001f52c6672c
Alexey Krivonogov <akrivonogov@gmail.com>
2016-12-08 20:30:38 +0500

    Added ajax_addresses

---
+++ packages/controllers/ajax_addresses.go
@@ -0,0 +1,59 @@
+// Copyright 2016 The go-daylight Authors
+// This file is part of the go-daylight library.
+//
+// The go-daylight library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-daylight library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-daylight library. If not, see <http://www.gnu.org/licenses/>.
+
+package controllers
+
+import (
+	//	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/EGaaS/go-egaas-mvp/packages/lib"
+	"github.com/EGaaS/go-egaas-mvp/packages/utils"
+)
+
+const AAddresses = `ajax_addresses`
+
+type AddressJson struct {
+	Address []string `json:"address"`
+	Error   string   `json:"error"`
+}
+
+func init() {
+	newPage(AAddresses, `json`)
+}
+
+func (c *Controller) AjaxAddresses() interface{} {
+	var (
+		result AddressJson
+		err    error
+		req    []map[string]string
+	)
+	result.Address = make([]string, 0)
+	addr := strings.Replace(c.r.FormValue(`address`), `-`, ``, -1)
+	ret, _ := strconv.ParseUint(addr+strings.Repeat(`0`, 20-len(addr)), 10, 64)
+	req, err = c.GetAll(`select id from "`+utils.Int64ToStr(c.SessStateId)+`_citizens" where id>=? order by id`, 7, int64(ret))
+
+	if err != nil {
+		result.Error = err.Error()
+	} else {
+		for _, ireq := range req {
+			id := uint64(utils.StrToInt64(ireq[`id`]))
+			result.Address = append(result.Address, lib.AddressToString(id))
+		}
+	}
+	return result
+}
