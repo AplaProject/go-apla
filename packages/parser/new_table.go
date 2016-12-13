aecc505fb11b86d3f7bc90cb1c8cac4e44a3cf2d
c-darwin <darwin84@yandex.ru>
2016-12-13 23:59:49 +0300

    upd interface

--- packages/parser/new_table.go
+++ packages/parser/new_table.go
@@ -112,7 +112,7 @@ func (p *Parser) NewTable() error {
 		colsSql += `"` + data[0] + `" `+colType+" ,\n"
 		colsSql2 += `"` + data[0] + `": "$citizen==` + citizenIdStr + `",`
 		if data[2] == "1" {
-			sqlIndex+=`CREATE INDEX "`+p.TxStateIDStr + `_` + p.TxMaps.String["table_name"] + `_` + data[0] + `_index" ON "` + p.TxStateIDStr + `_` + p.TxMaps.String["table_name"] + `" (` +data[0]+ `);`
+			sqlIndex+=`CREATE INDEX "`+tableName+ `_` + data[0] + `_index" ON "` + tableName + `" (` +data[0]+ `);`
 		}
 	}
 	colsSql2 = colsSql2[:len(colsSql2)-1]
