bfb29a812782a4d31fbb3af8826973fbe970ad3b
Alexey Krivonogov <akrivonogov@gmail.com>
2017-01-13 18:42:59 +0500

    Update lang cache

--- packages/parser/new_lang.go
+++ packages/parser/new_lang.go
@@ -84,6 +84,7 @@ func (p *Parser) NewLang() error {
 	if err != nil {
 		return p.ErrInfo(err)
 	}
+	utils.UpdateLang(int(p.TxStateID), p.TxMaps.String["name"], p.TxMaps.String["res"])
 	return nil
 }
 
