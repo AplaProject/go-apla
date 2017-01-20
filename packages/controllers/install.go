3b20d2009640f7a0954e41f7f395c0ef1feb9c86
c-darwin <darwin84@yandex.ru>
2017-01-21 00:03:46 +0300

    fixed clearing DB

--- packages/controllers/install.go
+++ packages/controllers/install.go
@@ -114,7 +114,15 @@ func (c *Controller) Install() (string, error) {
 		return "", utils.ErrInfo(err)
 	}
 
-	err = c.DCDB.ExecSql(`select 'drop table "' || tablename || '" cascade;' from pg_tables where schemaname = 'public';`)
+	err = c.DCDB.ExecSql(`
+	DO $$ DECLARE
+	    r RECORD;
+	BEGIN
+	    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
+		EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+	    END LOOP;
+	END $$;
+	`)
 	if err != nil {
 		log.Error("%v", utils.ErrInfo(err))
 		dropConfig()
