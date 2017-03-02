77ee95a184f2ea5dc4cbfcd4a37ede6d1c3e95ae
Alexey Krivonogov <akrivonogov@gmail.com>
2017-03-02 17:22:20 +0500

    Added  table tags in Tag text function

--- packages/utils/smart_u.go
+++ packages/utils/smart_u.go
@@ -792,7 +792,7 @@ func getTag(tag string, pars ...string) (out string) {
 
 func Tag(vars *map[string]string, pars ...string) (out string) {
 	var valid bool
-	for _, itag := range []string{`h1`, `h2`, `h3`, `h4`, `h5`, `button`} {
+	for _, itag := range []string{`h1`, `h2`, `h3`, `h4`, `h5`, `button`, `table`, `thead`, `tbody`, `tr`, `td`} {
 		if pars[0] == itag {
 			valid = true
 			break
