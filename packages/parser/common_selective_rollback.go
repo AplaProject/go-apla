47d45665bb77ef242612376a621ee27635028f2a
Ershavik <anna@egaas.org>
2017-05-24 22:18:37 +0300

    Update common_selective_rollback.go

--- packages/parser/common_selective_rollback.go
+++ packages/parser/common_selective_rollback.go
@@ -24,18 +24,21 @@ import (
 )
 
 // откат не всех полей, а только указанных, либо 1 строку, если нет where
+// the rollback not of all the fields but the specified ones or only 1 line if there is not 'where'
 func (p *Parser) selectiveRollback(table string, where string, rollbackAI bool) error {
 	if len(where) > 0 {
 		where = " WHERE " + where
 	}
 	tblname := lib.EscapeName(table)
 	// получим rb_id, по которому можно найти данные, которые были до этого
+	// we obtain rb_id with help of that it is possible to find the data which was before
 	rbId, err := p.Single("SELECT rb_id FROM " + tblname + " " + where + " order by rb_id desc").Int64()
 	if err != nil {
 		return p.ErrInfo(err)
 	}
 	if rbId > 0 {
 		// данные, которые восстановим
+		// data that we will be restored
 		rbData, err := p.OneRow("SELECT * FROM rollback WHERE rb_id  =  ?", rbId).String()
 		if err != nil {
 			return p.ErrInfo(err)
@@ -64,6 +67,7 @@ func (p *Parser) selectiveRollback(table string, where string, rollbackAI bool) 
 			return p.ErrInfo(err)
 		}
 		// подчищаем _log
+		// clean up the _log
 		err = p.ExecSql("DELETE FROM rollback WHERE rb_id = ?", rbId)
 		if err != nil {
 			return p.ErrInfo(err)
