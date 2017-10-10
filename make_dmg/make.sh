c757ed5bc4720b5f0a2d28c2d0d7854ef7d6f97b
Holodov Alexander <holodov.alexander@gmail.com>
2017-10-10 22:15:41 +0300

    removed make_dmg util

--- make_dmg/make.sh
+++
@@ -1,7 +0,0 @@
-diskutil unmount apla
-$GOPATH/bin/go-bindata -o="packages/static/static.go" -pkg="static" static/...
-GOARCH=amd64  CGO_ENABLED=1  go build -ldflags -s -o make_dmg/apla.app/Contents/MacOS/aplabin
-cd make_dmg
-zip -r apla_osx64.zip apla.app/Contents/MacOS/aplabin
-./make_dmg.sh -b background.png -i logo-big.icns -s "480:540" -c 240:400:240:200 -n apla_osx64 "apla.app"
-cd ../
