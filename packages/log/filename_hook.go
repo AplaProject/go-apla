c7182167e5dcf813339d73d2107b88ed49859cc5
Roman Potekhin <rpotekhin@mc2soft.ru>
2017-11-20 19:24:15 +0300

    Add from func field to filename hook

--- packages/log/filename_hook.go
+++ packages/log/filename_hook.go
@@ -16,19 +16,26 @@ func (hook ContextHook) Levels() []logrus.Level {
 }
 
 func (hook ContextHook) Fire(entry *logrus.Entry) error {
-	pc := make([]uintptr, 3, 3)
+	pc := make([]uintptr, 4, 4)
 	cnt := runtime.Callers(6, pc)
 
+	count := 0
 	for i := 0; i < cnt; i++ {
 		fu := runtime.FuncForPC(pc[i] - 1)
 		name := fu.Name()
 		if !strings.Contains(name, "github.com/sirupsen/logrus") {
 			file, line := fu.FileLine(pc[i] - 1)
-			entry.Data["file"] = path.Base(file)
-			entry.Data["func"] = path.Base(name)
-			entry.Data["line"] = line
-			entry.Data["time"] = time.Now().Format(time.RFC3339)
-			break
+			if count == 0 {
+				entry.Data["file"] = path.Base(file)
+				entry.Data["func"] = path.Base(name)
+				entry.Data["line"] = line
+				entry.Data["time"] = time.Now().Format(time.RFC3339)
+			}
+			if count == 1 {
+				entry.Data["from_func"] = path.Base(name)
+				break
+			}
+			count += 1
 		}
 	}
 	return nil
