fde36194577354c963570fed3aac75f8c0bc548a
Poletaev Roman <losaped@gmail.com>
2018-02-09 13:10:01 +0300

    add role support

--- packages/api/auth.go
+++ packages/api/auth.go
@@ -37,6 +37,7 @@ type JWTClaims struct {
 	UID         string `json:"uid,omitempty"`
 	EcosystemID string `json:"ecosystem_id,omitempty"`
 	KeyID       string `json:"key_id,omitempty"`
+	RoleID      string `json:"role_id,omitempty"`
 	jwt.StandardClaims
 }
 
