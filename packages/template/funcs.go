86045aaf2aed269528b1c468e3ef26bbefbfb78c
gentee <akrivonogov@gmail.com>
2018-03-12 10:32:46 +0500

    Fixed offset bug

--- packages/template/funcs.go
+++ packages/template/funcs.go
@@ -403,7 +403,9 @@ func dataTag(par parFunc) string {
 		for i, icol := range cols {
 			var ival string
 			if i < defcol {
-				ival = strings.TrimSpace(item[i])
+				if i < len(item) {
+					ival = strings.TrimSpace(item[i])
+				}
 				if strings.IndexByte(ival, '<') >= 0 {
 					ival = html.EscapeString(ival)
 				}
@@ -440,6 +442,7 @@ func dbfindTag(par parFunc) string {
 		state  int64
 		err    error
 		perm   map[string]string
+		offset string
 	)
 	if len((*par.Pars)[`Name`]) == 0 {
 		return ``
@@ -472,6 +475,10 @@ func dbfindTag(par parFunc) string {
 	if limit > 250 {
 		limit = 250
 	}
+	if par.Node.Attr[`offset`] != nil {
+		offset = fmt.Sprintf(` offset %d`, converter.StrToInt(par.Node.Attr[`offset`].(string)))
+	}
+
 	if par.Node.Attr[`prefix`] != nil {
 		prefix = par.Node.Attr[`prefix`].(string)
 		limit = 1
@@ -496,7 +503,7 @@ func dbfindTag(par parFunc) string {
 	}
 	fields = smart.PrepareColumns(fields)
 
-	list, err := model.GetAll(`select `+fields+` from "`+tblname+`"`+where+order, limit)
+	list, err := model.GetAll(`select `+fields+` from "`+tblname+`"`+where+order+offset, limit)
 	if err != nil {
 		log.WithFields(log.Fields{"type": consts.DBError, "error": err}).Error("getting all from db")
 		return err.Error()
