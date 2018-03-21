94a9c0994ade9387faf8c1f3995b80b7932d81f0
Poletaev Roman <losaped@gmail.com>
2018-03-21 21:14:59 +0300

    change model SystemState to Ecosystem add system_name to template vars change CreateEcosystem contract func

--- packages/model/system_states.go
+++ packages/model/system_states.go
@@ -1,18 +1,21 @@
 package model
 
-// SystemState is model
-type SystemState struct {
-	ID int64 `gorm:"primary_key;not null"`
+// Ecosystem is model
+type Ecosystem struct {
+	ID       int64 `gorm:"primary_key;not null"`
+	Name     string
+	IsValued bool
 }
 
 // TableName returns name of table
-func (ss *SystemState) TableName() string {
-	return "system_states"
+// only first ecosystem has this entity
+func (sys *Ecosystem) TableName() string {
+	return "1_ecosystems"
 }
 
 // GetAllSystemStatesIDs is retrieving all system states ids
 func GetAllSystemStatesIDs() ([]int64, error) {
-	states := new([]SystemState)
+	states := new([]Ecosystem)
 	if err := DBConn.Find(&states).Order("id").Error; err != nil {
 		return nil, err
 	}
@@ -23,7 +26,12 @@ func GetAllSystemStatesIDs() ([]int64, error) {
 	return ids, nil
 }
 
+// Get is fill reciever from db
+func (sys *Ecosystem) Get(id int64) error {
+	return DBConn.First(sys, "id = ?", id).Error
+}
+
 // Delete is deleting record
-func (ss *SystemState) Delete(transaction *DbTransaction) error {
-	return GetDB(transaction).Delete(ss).Error
+func (sys *Ecosystem) Delete(transaction *DbTransaction) error {
+	return GetDB(transaction).Delete(sys).Error
 }
