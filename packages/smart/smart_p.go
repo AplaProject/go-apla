22fc129cf446ce111f663166a36449846ad48fb9
Roman Potekhin <rpotekhin@mc2soft.ru>
2018-04-10 16:17:41 +0300

    Fix bug with rollback: wrong drop table list

--- packages/smart/smart_p.go
+++ packages/smart/smart_p.go
@@ -507,8 +507,8 @@ func RollbackEcosystem(sc *SmartContract) error {
 	}
 
 	for _, name := range []string{`menu`, `pages`, `languages`, `signatures`, `tables`,
-		`contracts`, `parameters`, `blocks`, `history`, `keys`, `sections`, `members`, `roles_list`,
-		`roles_assign`, `notifications`, `applications`, `binaries`, `app_param`} {
+		`contracts`, `parameters`, `blocks`, `history`, `keys`, `sections`, `members`, `roles`,
+		`roles_participants`, `notifications`, `applications`, `binaries`, `app_param`} {
 		err = model.DropTable(sc.DbTransaction, fmt.Sprintf("%s_%s", rollbackTx.TableID, name))
 		if err != nil {
 			log.WithFields(log.Fields{"type": consts.DBError, "error": err}).Error("dropping table")
