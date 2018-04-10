5d73d2e84e3921b31675ef2582169bb025981131
Poletaev Roman <losaped@gmail.com>
2018-04-10 14:10:02 +0300

    add ecosystem parameter to path

--- packages/api/route.go
+++ packages/api/route.go
@@ -73,7 +73,7 @@ func Route(route *hr.Router) {
 	get(`block/:id`, ``, getBlockInfo)
 	get(`maxblockid`, ``, getMaxBlockID)
 	get(`version`, ``, getVersion)
-	get(`avatar/:member`, ``, getAvatar)
+	get(`avatar/:ecosystem/:member`, ``, getAvatar)
 	post(`content/source/:name`, ``, authWallet, getSource)
 	post(`content/page/:name`, `?lang:string`, authWallet, getPage)
 	post(`content/menu/:name`, `?lang:string`, authWallet, getMenu)
@@ -90,6 +90,7 @@ func Route(route *hr.Router) {
 	post(`updnotificator`, `ids:string`, updateNotificator)
 
 	methodRoute(route, `POST`, `node/:name`, `?token_ecosystem:int64,?max_sum ?payover:string`, nodeContract)
+	// route.Handle("GET", "avatar/:ecosystem/:member", getAvatar)
 }
 
 func processParams(input string) (params map[string]int) {
