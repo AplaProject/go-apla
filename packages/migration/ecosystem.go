3c4b3414c1094f457112c2cb8122892755f3b853
Roman Poletaev <losaped@gmail.com>
2018-04-20 11:36:20 +0300

    requested changes

--- packages/migration/ecosystem.go
+++ packages/migration/ecosystem.go
@@ -1558,12 +1558,8 @@ MenuItem(
 			}
 			var total money
 			$amount = Money($Amount) 
-			if $amount == 0 {
-				error "Amount is zero"
-			}
-
-			if $amount < 0 {
-				error "Amount must be greater then 0"
+			if $amount <= 0 {
+				error "Amount must be greater then zero"
 			}
 
 			var row map
