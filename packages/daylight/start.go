f6bfd4638ed2dc89cea487f13b0306cdea21f438
yddmat <yddmat@gmail.com>
2018-04-23 04:35:56 +0300

    Config generation from env added

--- packages/daylight/start.go
+++ packages/daylight/start.go
@@ -80,18 +80,18 @@ func killOld() {
 }
 
 func initLogs() error {
-	switch conf.Config.LogConfig.LogFormat {
+	switch conf.Config.Log.LogFormat {
 	case "json":
 		log.SetFormatter(&log.JSONFormatter{})
 	default:
 		log.SetFormatter(&log.TextFormatter{})
 	}
-	switch conf.Config.LogConfig.LogTo {
+	switch conf.Config.Log.LogTo {
 	case "stdout":
 		log.SetOutput(os.Stdout)
 	case "syslog":
-		facility := conf.Config.LogConfig.Syslog.Facility
-		tag := conf.Config.LogConfig.Syslog.Tag
+		facility := conf.Config.Log.Syslog.Facility
+		tag := conf.Config.Log.Syslog.Tag
 		sysLogHook, err := logtools.NewSyslogHook(tag, facility)
 		if err != nil {
 			log.WithError(err).Error("initializing syslog hook")
@@ -99,7 +99,7 @@ func initLogs() error {
 			log.AddHook(sysLogHook)
 		}
 	default:
-		fileName := filepath.Join(conf.Config.DataDir, conf.Config.LogConfig.LogTo)
+		fileName := filepath.Join(conf.Config.DataDir, conf.Config.Log.LogTo)
 		openMode := os.O_APPEND
 		if _, err := os.Stat(fileName); os.IsNotExist(err) {
 			openMode = os.O_CREATE
@@ -113,7 +113,7 @@ func initLogs() error {
 		log.SetOutput(f)
 	}
 
-	switch conf.Config.LogConfig.LogLevel {
+	switch conf.Config.Log.LogLevel {
 	case "DEBUG":
 		log.SetLevel(log.DebugLevel)
 	case "INFO":
