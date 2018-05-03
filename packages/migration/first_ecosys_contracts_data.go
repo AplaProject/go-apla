c68c1d085f815ae50a1fd42ebb8125e0fcb73457
gentee <akrivonogov@gmail.com>
2018-05-03 17:12:28 +0500

    Fixed NewUser contract

--- packages/migration/first_ecosys_contracts_data.go
+++ packages/migration/first_ecosys_contracts_data.go
@@ -1808,6 +1808,7 @@ VALUES ('2', 'DelApplication', 'contract DelApplication {
 	}
 	action {
         MoneyTransfer("Recipient,Amount,Comment", Str($newId), Str($amount), "New user deposit")
+        DBUpdate("keys", $newId, "pub", $NewPubkey)
 	}
 }', %[1]d, 'ContractConditions("NodeOwnerCondition")', 1),
 ('41', 'EditEcosystemName','contract EditEcosystemName {
