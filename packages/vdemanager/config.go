b928afd97c2a6e9e837b5ca69a11c2ee9598972b
Roman Poletaev <losaped@gmail.com>
2018-05-07 15:59:38 +0300

    temp commit

--- packages/vdemanager/config.go
+++ packages/vdemanager/config.go
@@ -31,33 +31,33 @@ func (c ChildVDEConfig) configCommand() *exec.Cmd {
 		fmt.Sprintf("--dbUser=%s", c.DBUser),
 		fmt.Sprintf("--dbPassword=%s", c.DBPassword),
 		fmt.Sprintf("--dbName=%s", c.Name),
-		fmt.Sprintf("--httpPort=%d", c.HTTPPort)
+		fmt.Sprintf("--httpPort=%d", c.HTTPPort),
 		fmt.Sprintf("--dataDir=%s", c.Directory),
 		fmt.Sprintf("--keysDir=%s", c.Directory),
-		fmt.Sprintf("--runMode=VDE")
+		"--runMode=VDE",
 	}
 
 	return exec.Command(c.Executable, args...)
 }
 
-func (c ChildVDEConfig) initDBCommand() exec.Cmd {
-	return getCommand(inidDBCommand)
+func (c ChildVDEConfig) initDBCommand() *exec.Cmd {
+	return c.getCommand(inidDBCommand)
 }
 
-func (c ChildVDEConfig) generateKeysCommand() exec.Cmd {
-	return getCommand(genKeysCommand)
+func (c ChildVDEConfig) generateKeysCommand() *exec.Cmd {
+	return c.getCommand(genKeysCommand)
 }
 
-func (c ChildVDEConfig) startCommand() exec.Cmd {
-	retturn getCommand(startCommand)
+func (c ChildVDEConfig) startCommand() *exec.Cmd {
+	return c.getCommand(startCommand)
 }
 
 func (c ChildVDEConfig) configPath() string {
-	return filepath.Join(c.Directory, ConfigFileName)
+	return filepath.Join(c.Directory, c.ConfigFileName)
 }
 
-func (c ChildVDEConfig) getCommand(commandName string)  *exec.Cmd {
-	return args := []string{
+func (c ChildVDEConfig) getCommand(commandName string) *exec.Cmd {
+	args := []string{
 		commandName,
 		fmt.Sprintf("--config=%s", c.configPath()),
 	}
