11c130cd65daccf07a68ec3c4b7ca777b7ad6033
Roman Poletaev <losaped@gmail.com>
2018-05-07 13:37:49 +0300

    separate routes by vde

--- packages/api/route.go
+++ packages/api/route.go
@@ -93,7 +93,6 @@ func Route(route *hr.Router) {
 		get(`balance/:wallet`, `?ecosystem:int64`, authWallet, balance)
 		get(`block/:id`, ``, getBlockInfo)
 		get(`maxblockid`, ``, getMaxBlockID)
-
 		get(`ecosystemparams`, `?ecosystem:int64,?names:string`, authWallet, ecosystemParams)
 		get(`systemparams`, `?names:string`, authWallet, systemParams)
 		get(`ecosystems`, ``, authWallet, ecosystems)
