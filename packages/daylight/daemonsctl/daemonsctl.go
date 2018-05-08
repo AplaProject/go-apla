e7c0a7e37a4e1b36f175033b3c34cfd26f828f93
Roman Poletaev <losaped@gmail.com>
2018-05-08 09:59:10 +0300

    temporary commit

--- packages/daylight/daemonsctl/daemonsctl.go
+++ packages/daylight/daemonsctl/daemonsctl.go
@@ -14,17 +14,19 @@ import (
 
 // RunAllDaemons start daemons, load contracts and tcpserver
 func RunAllDaemons() error {
-	logEntry := log.WithFields(log.Fields{"daemon_name": "block_collection"})
+	if !conf.Config.IsSupportingVDE() {
+		logEntry := log.WithFields(log.Fields{"daemon_name": "block_collection"})
 
-	daemons.InitialLoad(logEntry)
-	err := syspar.SysUpdate(nil)
-	if err != nil {
-		log.Errorf("can't read system parameters: %s", utils.ErrInfo(err))
-		return err
-	}
+		daemons.InitialLoad(logEntry)
+		err := syspar.SysUpdate(nil)
+		if err != nil {
+			log.Errorf("can't read system parameters: %s", utils.ErrInfo(err))
+			return err
+		}
 
-	if data, ok := parser.GetDataFromFirstBlock(); ok {
-		syspar.SetFirstBlockData(data)
+		if data, ok := parser.GetDataFromFirstBlock(); ok {
+			syspar.SetFirstBlockData(data)
+		}
 	}
 
 	log.Info("load contracts")
@@ -36,8 +38,7 @@ func RunAllDaemons() error {
 	log.Info("start daemons")
 	daemons.StartDaemons()
 
-	err = tcpserver.TcpListener(conf.Config.TCPServer.Str())
-	if err != nil {
+	if err := tcpserver.TcpListener(conf.Config.TCPServer.Str()); err != nil {
 		log.Errorf("can't start tcp servers, stop")
 		return err
 	}
