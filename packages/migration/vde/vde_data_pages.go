48174f61fceca872d7303e9041589c9af185b069
Roman Poletaev <losaped@gmail.com>
2018-05-10 22:37:36 +0300

    fix login

--- packages/migration/vde/vde_data_pages.go
+++ packages/migration/vde/vde_data_pages.go
@@ -1,5 +1,5 @@
 package vde
 
 var pagesDataSQL = `
-INSERT INTO "%[1]d_pages" ("id","name","value","menu","conditions") VALUES('2','admin_index','','admin_menu','true');
+INSERT INTO "%[1]d_pages" ("id","name","value","menu","conditions") VALUES('1', 'default_page', '', 'admin_menu', 'true'),('2','admin_index','','admin_menu','true');
 `
