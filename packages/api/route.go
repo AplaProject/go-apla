6820eca2f544103175ee74cc6dc254b787d06ba3
Roman Poletaev <losaped@gmail.com>
2018-05-25 11:43:58 +0300

    add api endpoint for getitng ecosystem_name

--- packages/api/route.go
+++ packages/api/route.go
@@ -76,6 +76,7 @@ func Route(route *hr.Router) {
 	get(`version`, ``, getVersion)
 	get(`avatar/:ecosystem/:member`, ``, getAvatar)
 	get(`config/:option`, ``, getConfigOption)
+	get("ecosystemname", "?id:int64", getEcosystemName)
 	post(`content/source/:name`, ``, authWallet, getSource)
 	post(`content/page/:name`, `?lang:string`, authWallet, getPage)
 	post(`content/menu/:name`, `?lang:string`, authWallet, getMenu)
