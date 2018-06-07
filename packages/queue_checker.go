756f4ebc8a8e70fb3885da3edf0c66b92919fb7d
Roman Poletaev <losaped@gmail.com>
2018-06-07 15:50:16 +0300

    eoud

--- packages/queue_checker.go
+++ packages/queue_checker.go
@@ -1,6 +1,14 @@
 package packages
 
+import (
+	"time"
+)
+
 // QueueChecker allow check queue to generate current block
 type QueueChecker interface {
 	TimeToGenerate(position int64) (bool, error)
+	ValidateBlock(position int64, at time.Time) (bool, error)
+	NextTime(position int64, t time.Time) time.Time
+	BlockForTimeExists(t time.Time, nodePosition int) (bool, error)
+	RangesByTime(t time.Time) (start, end time.Time)
 }
