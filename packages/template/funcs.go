072f46b3fcc17a608b14d0c2bd5c32b331bfffa3
gentee <akrivonogov@gmail.com>
2018-06-14 14:58:03 +0500

    Added macro to include

--- packages/template/funcs.go
+++ packages/template/funcs.go
@@ -816,7 +816,8 @@ func tailTag(par parFunc) string {
 
 func includeTag(par parFunc) string {
 	if len((*par.Pars)[`Name`]) >= 0 && len((*par.Workspace.Vars)[`_include`]) < 5 {
-		pattern, err := model.Single(`select value from "`+(*par.Workspace.Vars)[`ecosystem_id`]+`_blocks" where name=?`, (*par.Pars)[`Name`]).String()
+		pattern, err := model.Single(`select value from "`+(*par.Workspace.Vars)[`ecosystem_id`]+
+			`_blocks" where name=?`, macro((*par.Pars)[`Name`], par.Workspace.Vars)).String()
 		if err != nil {
 			log.WithFields(log.Fields{"type": consts.DBError, "error": err}).Error("getting block by name")
 			return err.Error()
