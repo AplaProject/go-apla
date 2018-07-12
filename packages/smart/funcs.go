aa0b192dc71fdfbb0f9596cbf2fc60c0e4e6d552
Roman Poletaev <losaped@gmail.com>
2018-07-12 15:21:14 +0300

    add func MemoryLeak that allow drop node

--- packages/smart/funcs.go
+++ packages/smart/funcs.go
@@ -256,6 +256,7 @@ func EmbedFuncs(vm *script.VM, vt script.VMType) {
 		"GetBlockHistory":              GetBlockHistory,
 		"GetMenuHistory":               GetMenuHistory,
 		"GetContractHistory":           GetContractHistory,
+		"MemoryLeak":                   MemoryLeak,
 	}
 
 	switch vt {
@@ -1842,6 +1843,10 @@ func GetPageHistory(sc *SmartContract, id int64) ([]interface{}, error) {
 	return GetHistory(sc.DbTransaction, sc.TxSmart.EcosystemID, `pages`, id)
 }
 
+func MemoryLeak(sc *SmartContract) error {
+	MemoryLeak(sc)
+	return nil
+}
 func GetMenuHistory(sc *SmartContract, id int64) ([]interface{}, error) {
 	return GetHistory(sc.DbTransaction, sc.TxSmart.EcosystemID, `menu`, id)
 }
