2512761cab58835d3e31a8392fee8846e19b1a23
Dmitriy Chertkov <dmitriychertkov@gmail.com>
2018-07-20 14:26:28 +0500

    Fixed role for first ecosystem

--- packages/migration/first_ecosystems_data.go
+++ packages/migration/first_ecosystems_data.go
@@ -7,6 +7,10 @@ UPDATE "1_roles"
 	SET role_name = 'Platform Administrator'
 	WHERE id = 1;
 
+UPDATE "1_roles_participants"
+	SET role = '{"id": "1", "type": "3", "name": "Platform Administrator", "image_id":"0"}'
+	WHERE id = 1;
+
 INSERT INTO "1_applications" (id, name, conditions) VALUES (2, 'System parameters', 
 	'ContractConditions("MainCondition")');
 `
