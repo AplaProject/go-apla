ee99908f48f94716c47635a593a67b1214387664
CynepHy6 <CynepHy6@gmail.com>
2018-08-16 18:32:41 +0300

    fixed moved roles

--- packages/migration/first_ecosystems_data.go
+++ packages/migration/first_ecosystems_data.go
@@ -2,12 +2,4 @@ package migration
 
 var firstEcosystemDataSQL = `
 INSERT INTO "1_ecosystems" ("id", "name", "is_valued") VALUES ('1', 'platform ecosystem', 0);
-
-INSERT INTO "1_roles" ("id", "default_page", "role_name", "deleted", "role_type",
-	"date_created","creator","roles_access") VALUES
-	('3','', 'Apla Consensus asbl', '0', '3', NOW(), '{}', '{"rids": "1"}'),
-	('4','', 'Candidate for validators', '0', '3', NOW(), '{}', '{"rids": "1"}'),
-	('5','', 'Validator', '0', '3', NOW(), '{}', '{"rids": "1"}'),
-	('6','', 'Investor with voting rights', '0', '3', NOW(), '{}', '{"rids": "1"}'),
-	('7','', 'Delegate', '0', '3', NOW(), '{}', '{"rids": "1"}');
 `
