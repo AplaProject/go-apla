1c6e29a5378b7b2e36017197410c88c6e4363496
Evgeny Lerner <dvork1ng.egaas@gmail.com>
2018-08-18 13:47:23 +0300

    E_VDE API error removed. It is unused.

--- packages/api/errors.go
+++ packages/api/errors.go
@@ -48,7 +48,6 @@ var (
 		`E_UNAUTHORIZED`:    `Unauthorized`,
 		`E_UNDEFINEVAL`:     `Value %s is undefined`,
 		`E_UNKNOWNUID`:      `Unknown uid`,
-		`E_VDE`:             `Virtual Dedicated Ecosystem %d doesn't exist`,
 		`E_VDECREATED`:      `Virtual Dedicated Ecosystem is already created`,
 		`E_REQUESTNOTFOUND`: `Request %s doesn't exist`,
 		`E_UPDATING`:        `Node is updating blockchain`,
