ae3f339711acc5a927fd90e13d61b95419d25832
gentee <akrivonogov@gmail.com>
2018-08-21 11:04:25 +0500

    Added vde import

--- packages/migration/roles_data.go
+++ packages/migration/roles_data.go
@@ -1,5 +1,9 @@
 package migration
 
+import (
+	"github.com/GenesisKernel/go-genesis/packages/migration/vde"
+)
+
 var rolesDataSQL = `
 INSERT INTO "%[1]d_roles" ("id", "default_page", "role_name", "deleted", "role_type",
 	"date_created","creator","roles_access") VALUES
@@ -11,6 +15,6 @@ INSERT INTO "%[1]d_roles" ("id", "default_page", "role_name", "deleted", "role_t
 	('2', '{"id": "2", "type": "3", "name": "Developer", "image_id":"0"}', '{"member_id": "%[2]d", "member_name": "founder", "image_id": "0"}', NOW());
 
 	INSERT INTO "%[1]d_members" ("id", "member_name") VALUES('%[2]d', 'founder'),
-	('` + GuestKey + `', 'guest');
+	('` + vde.GuestKey + `', 'guest');
 
 `
