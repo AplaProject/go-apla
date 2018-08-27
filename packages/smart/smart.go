5f56e4a3aa63547c84da5d6265e311d30a9a8f25
gentee <akrivonogov@gmail.com>
2018-08-27 13:56:05 +0500

    Moved buffer_data and app_params to the first ecosystem

--- packages/smart/smart.go
+++ packages/smart/smart.go
@@ -554,7 +554,7 @@ func (sc *SmartContract) AccessTablePerm(table, action string) (map[string]strin
 	)
 	logger := sc.GetLogger()
 	isRead := action == `read`
-	if model.RealName(table) == `1_parameters` || table == getDefTableName(sc, `app_params`) {
+	if model.RealName(table) == `1_parameters` || model.RealName(table) == `1_app_params` {
 		if isRead || sc.TxSmart.KeyID == converter.StrToInt64(EcosysParam(sc, `founder_account`)) {
 			return tablePermission, nil
 		}
@@ -617,7 +617,7 @@ func (sc *SmartContract) AccessColumns(table string, columns *[]string, update b
 	if sc.FullAccess {
 		return nil
 	}
-	if table == getDefTableName(sc, `parameters`) || table == getDefTableName(sc, `app_params`) {
+	if model.RealName(table) == `1_parameters` || model.RealName(table) == `1_app_params` {
 		if update {
 			if sc.TxSmart.KeyID == converter.StrToInt64(EcosysParam(sc, `founder_account`)) {
 				return nil
