c7328e5e01402f9f178d91ddbdafd56cdfc42fd1
gentee <akrivonogov@gmail.com>
2018-08-27 10:39:54 +0500

    Moved roles to the first ecosystem

--- packages/migration/sections_data.go
+++ packages/migration/sections_data.go
@@ -2,6 +2,6 @@ package migration
 
 var sectionsDataSQL = `
 INSERT INTO "1_sections" ("id","title","urlname","page","roles_access", "status", "ecosystem") VALUES
-('1', 'Home', 'home', 'default_page', '[]', 2, '%[1]d'),
-('2', 'Developer', 'admin', 'admin_index', '[]', 1, '%[1]d');
+(next_id('1_sections'), 'Home', 'home', 'default_page', '[]', 2, '%[1]d'),
+(next_id('1_sections'), 'Developer', 'admin', 'admin_index', '[]', 1, '%[1]d');
 `
