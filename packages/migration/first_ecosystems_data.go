8abc677a19fcb37b4b000a180de1b7a784c5d3d0
gentee <akrivonogov@gmail.com>
2018-08-28 10:11:25 +0500

    Removed predeclared roles

--- packages/migration/first_ecosystems_data.go
+++ packages/migration/first_ecosystems_data.go
@@ -2,12 +2,4 @@ package migration
 
 var firstEcosystemDataSQL = `
 INSERT INTO "1_ecosystems" ("id", "name", "is_valued") VALUES ('1', 'platform ecosystem', 0);
-
-INSERT INTO "1_roles" ("id", "default_page", "role_name", "deleted", "role_type",
-	"date_created","creator","roles_access", "ecosystem") VALUES
-	(next_id('1_roles'),'', 'Apla Consensus asbl', '0', '3', NOW(), '{}', '{"rids": "1"}', '1'),
-	(next_id('1_roles'),'', 'Candidate for validators', '0', '3', NOW(), '{}', '{"rids": "1"}', '1'),
-	(next_id('1_roles'),'', 'Validator', '0', '3', NOW(), '{}', '{"rids": "1"}', '1'),
-	(next_id('1_roles'),'', 'Investor with voting rights', '0', '3', NOW(), '{}', '{"rids": "1"}', '1'),
-	(next_id('1_roles'),'', 'Delegate', '0', '3', NOW(), '{}', '{"rids": "1"}', '1');
 `
