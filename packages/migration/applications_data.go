119e626f2dbe048cfab2a49edbb721273de710df
gentee <akrivonogov@gmail.com>
2018-08-30 11:00:56 +0500

    Fixed default application data

--- packages/migration/applications_data.go
+++
@@ -1,3 +0,0 @@
-package migration
-
-var applicationsDataSQL = `INSERT INTO "1_applications" (id, name, conditions, ecosystem) VALUES (next_id('1_applications'), 'System', 'ContractConditions("MainCondition")', '%[1]d');`
