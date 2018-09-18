d941d3f85836df46328667d8db59b1828fe4f4fd
Roman Poletaev <losaped@gmail.com>
2018-09-18 09:22:16 +0300

    bench with profileng data

--- packages/network/protocol_test.go
+++ packages/network/protocol_test.go
@@ -2,6 +2,8 @@ package network
 
 import (
 	"bytes"
+	"encoding/binary"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -16,4 +18,43 @@ func TestEmptyBodyResponse(t *testing.T) {
 	r := bytes.NewReader(w.Bytes())
 	emptyRes := &GetBodyResponse{}
 	require.NoError(t, emptyRes.Read(r))
+}
+
+func TestWriteReadInts(t *testing.T) {
+	buf := []byte{}
+	b := bytes.NewBuffer(buf)
+	st := uint16(2)
+	require.NoError(t, binary.Write(b, binary.LittleEndian, st))
+
+	var val uint16
+	err := binary.Read(b, binary.LittleEndian, &val)
+	require.NoError(t, err)
+	require.Equal(t, val, st)
+	fmt.Println(val)
+}
+
+func TestRequestType(t *testing.T) {
+	rt := RequestType{1}
+	buf := []byte{}
+	b := bytes.NewBuffer(buf)
+
+	result := RequestType{}
+	require.NoError(t, rt.Write(b))
+	require.NoError(t, result.Read(b))
+	require.Equal(t, rt, result)
+	fmt.Println(rt, result)
+
+}
+
+func TestGetBodyResponse(t *testing.T) {
+	rt := GetBodyResponse{Data: make([]byte, 4, 4)}
+	buf := []byte{}
+	b := bytes.NewBuffer(buf)
+
+	result := GetBodyResponse{}
+	require.NoError(t, rt.Write(b))
+	require.NoError(t, result.Read(b))
+	require.Equal(t, rt, result)
+	fmt.Println(rt, result)
+
 }
---
+++ packages/network/tcpclient/client_test.go
@@ -0,0 +1,117 @@
+package tcpclient
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	_ "net/http/pprof"
+	"strings"
+	"testing"
+
+	"github.com/GenesisKernel/go-genesis/packages/network"
+)
+
+func init() {
+	InitBlockBodyBuffer(7, 60)
+}
+
+type BufCloser struct {
+	*bytes.Buffer
+}
+
+func (bc BufCloser) Close() error {
+	bc.Truncate(0)
+	return nil
+}
+
+// 4 bytes length
+//100000	     19376 ns/op	     488 B/op	      31 allocs/op
+//100000	     18469 ns/op	     449 B/op	      26 allocs/op
+// 32 bytes length
+// 100000	     19966 ns/op	     537 B/op	      26 allocs/op
+// with stopTimer on init
+// 50000	     28630 ns/op	     466 B/op	      16 allocs/op
+//========================
+// BenchmarkGetBlockBodies-8   	16000
+// 1600000
+// 16000000
+//   100000	     17380 ns/op	     577 B/op	      19 allocs/op
+// PASS
+// ok  	github.com/GenesisKernel/go-genesis/packages/network/tcpclient	8.976s
+func BenchmarkGetBlockBodies(t *testing.B) {
+	ctx := context.Background()
+	var bts []byte
+	r := BufCloser{
+		Buffer: bytes.NewBuffer(bts),
+	}
+
+	byteString := []byte(strings.Repeat("A", 32))
+	var lenCounter int
+	t.ResetTimer()
+	for j := 0; j < t.N; j++ {
+		t.StopTimer()
+		for i := 0; i < 5; i++ {
+			resp := network.GetBodyResponse{
+				Data: byteString,
+			}
+
+			resp.Write(r)
+		}
+
+		t.StartTimer()
+		blocksC, errC := GetBlockBodiesChan(ctx, r, 5)
+
+		go func() {
+			err := <-errC
+			if err != nil {
+				fmt.Println(err)
+			}
+		}()
+
+		for item := range blocksC {
+			lenCounter += len(item)
+		}
+	}
+
+	fmt.Println(lenCounter)
+}
+
+func BenchmarkGetBlockBodiesWithBuffer(t *testing.B) {
+	ctx := context.Background()
+	var bts []byte
+	r := BufCloser{
+		Buffer: bytes.NewBuffer(bts),
+	}
+
+	byteString := []byte(strings.Repeat("A", 32))
+	var lenCounter int
+	t.ResetTimer()
+	for j := 0; j < t.N; j++ {
+		t.StopTimer()
+		for i := 0; i < 5; i++ {
+			resp := network.GetBodyResponse{
+				Data: byteString,
+			}
+
+			resp.Write(r)
+		}
+
+		t.StartTimer()
+
+		blocksC, errC := GetBlockBodiesChanWithPool(ctx, r, 5)
+
+		go func() {
+			err := <-errC
+			if err != nil {
+				fmt.Println(err)
+			}
+		}()
+
+		for item := range blocksC {
+			lenCounter += len(item)
+			BlockBodyPool.putBytes(item)
+		}
+	}
+
+	fmt.Println(lenCounter)
+}
