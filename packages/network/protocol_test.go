ae2846e660dbffb74cae90230a0a8c18d57c3603
Roman Poletaev <losaped@gmail.com>
2018-09-25 10:58:21 +0300

    test to check changed format

--- packages/network/protocol_test.go
+++ packages/network/protocol_test.go
@@ -4,12 +4,13 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestEmptyBodyResponse(t *testing.T) {
+func TestEmptyGetBodyResponse(t *testing.T) {
 	buf := []byte{}
 	w := bytes.NewBuffer(buf)
 	empty := &GetBodyResponse{}
@@ -55,6 +56,19 @@ func TestGetBodyResponse(t *testing.T) {
 	require.NoError(t, rt.Write(b))
 	require.NoError(t, result.Read(b))
 	require.Equal(t, rt, result)
+	fmt.Println(rt, result)
+
+}
+
+func TestBodyResponse(t *testing.T) {
+	rt := GetBodyResponse{Data: []byte(strings.Repeat("A", 32))}
+	buf := []byte{}
+	b := bytes.NewBuffer(buf)
+
+	result := &GetBodyResponse{}
+	require.NoError(t, rt.Write(b))
+	require.NoError(t, result.Read(b))
+	require.Equal(t, rt.Data, result.Data)
 	fmt.Println(rt, result)
 
 }
