ed7da25e85b23654cb73f4d5fb88fa7b48edd0ab
Roman Poletaev <losaped@gmail.com>
2018-10-08 15:51:45 +0300

    remove unused consts

--- packages/migration/ecosystem.go
+++ packages/migration/ecosystem.go
@@ -4,13 +4,6 @@ import (
 	"strings"
 )
 
-const (
-	// GuestKey is the guest id
-	GuestKey = `4544233900443112470`
-	// GuestPublic is the public guest key
-	GuestPublic = `489347a1205c818d9a02f285faaedd0122a56138e3d985f5e1b4f6a9470f90f692a00a3453771dd7feea388ceb7aefeaf183e299c70ad1aecb7f870bfada3b86`
-)
-
 // GetEcosystemScript returns script to create ecosystem
 func GetEcosystemScript() string {
 	scripts := []string{
