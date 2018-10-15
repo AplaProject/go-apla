1c19b54ce6ae4a721e52d2042e33f53f69443fba
Roman Poletaev <losaped@gmail.com>
2018-10-15 18:39:00 +0300

    set guest pub key as empty sttring

--- packages/migration/first_keys_data.go
+++ packages/migration/first_keys_data.go
@@ -2,4 +2,4 @@ package migration
 
 import "github.com/GenesisKernel/go-genesis/packages/consts"
 
-var firstKeysDataSQL = `INSERT INTO "1_keys" (id, pub, blocked, ecosystem) VALUES (` + consts.GuestKey + `, '` + consts.GuestPublic + `', 1, '1');`
+var firstKeysDataSQL = `INSERT INTO "1_keys" (id, pub, blocked, ecosystem) VALUES (` + consts.GuestKey + `, '', 1, '1');`
