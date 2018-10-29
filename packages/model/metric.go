e59dee8f770a24cea6ecd091d4dc4ac6601abc09
Alexey Krivonogov <akrivonogov@gmail.com>
2018-10-29 09:16:15 +0500

    hotfix/1241-metrics (#584)
    * Fixed metrics
    
    * Fixed GetMetricValues

--- packages/model/metric.go
+++ packages/model/metric.go
@@ -1,5 +1,9 @@
 package model
 
+import (
+	"time"
+)
+
 const tableNameMetrics = "1_metrics"
 
 // Metric represents record of system_metrics table
@@ -25,14 +29,15 @@ type EcosystemTx struct {
 
 // GetEcosystemTxPerDay returns the count of transactions per day for ecosystems,
 // processes data for two days
-func GetEcosystemTxPerDay() ([]*EcosystemTx, error) {
+func GetEcosystemTxPerDay(timeBlock int64) ([]*EcosystemTx, error) {
+	curDate := time.Unix(timeBlock, 0).Format(`2006-01-02`)
 	sql := `SELECT
 		EXTRACT(EPOCH FROM to_timestamp(bc.time)::date)::int "unix_time",
 		SUBSTRING(rtx.table_name FROM '^\d+') "ecosystem",
 		COUNT(*)
 	FROM rollback_tx rtx
 		INNER JOIN block_chain bc ON bc.id = rtx.block_id
-	WHERE to_timestamp(bc.time)::date >= current_date-1
+	WHERE to_timestamp(bc.time)::date >= (DATE('` + curDate + `') - interval '1' day)::date
 	GROUP BY unix_time, ecosystem`
 
 	var ecosystemTx []*EcosystemTx
@@ -45,9 +50,10 @@ func GetEcosystemTxPerDay() ([]*EcosystemTx, error) {
 }
 
 // GetMetricValues returns aggregated metric values in the time interval
-func GetMetricValues(metric, timeInterval, aggregateFunc string) ([]interface{}, error) {
+func GetMetricValues(metric, timeInterval, aggregateFunc, timeBlock string) ([]interface{}, error) {
 	rows, err := DBConn.Table(tableNameMetrics).Select("key,"+aggregateFunc+"(value)").
-		Where("metric = ? AND time >= EXTRACT(EPOCH FROM NOW() - CAST(? AS INTERVAL))", metric, timeInterval).
+		Where("metric = ? AND time >= EXTRACT(EPOCH FROM TIMESTAMP '"+timeBlock+"' - CAST(? AS INTERVAL))",
+			metric, timeInterval).
 		Group("key").Rows()
 	if err != nil {
 		return nil, err
