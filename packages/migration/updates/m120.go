aef30e965365800bc441362e1c3288402c8de4e3
gentee <akrivonogov@gmail.com>
2018-11-02 15:43:15 +0500

    Fixed craete_at in history

---
+++ packages/migration/updates/m120.go
@@ -0,0 +1,8 @@
+package updates
+
+var M120 = `
+ALTER TABLE "1_history" ADD COLUMN "created_new" bigint NOT NULL DEFAULT '1';
+UPDATE "1_history" SET created_new = EXTRACT(EPOCH FROM created_at::timestamp with time zone);
+ALTER TABLE "1_history" DROP COLUMN "created_at";
+ALTER TABLE "1_history" RENAME COLUMN "created_new" TO "created_at";
+`
