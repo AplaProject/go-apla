096fdb2055306d383d717a7a5ea20009663e00e3
Vasily Starovetskiy <syypoo@gmail.com>
2018-11-09 21:11:55 +1000

    Update migration (#597)

--- packages/migration/menu_data.go
+++ packages/migration/menu_data.go
@@ -1,6 +1,6 @@
 package migration
 
 var menuDataSQL = `INSERT INTO "1_menu" (id, name, value, conditions, ecosystem) VALUES
-(next_id('1_menu'), 'admin_menu', '', 'ContractAccess("@1EditMenu")','%[1]d'),
-(next_id('1_menu'), 'developer_menu', 'MenuItem(Title:"Import", Page:@1import_upload, Icon:"icon-cloud-upload")', 'ContractAccess("@1EditMenu")','%[1]d');
+	(next_id('1_menu'), 'admin_menu', '', 'ContractConditions("@1DeveloperCondition")', '%[1]d'),
+	(next_id('1_menu'), 'developer_menu', 'MenuItem(Title:"Import", Page:@1import_upload, Icon:"icon-cloud-upload")', 'ContractConditions("@1DeveloperCondition")', '%[1]d');
 `
