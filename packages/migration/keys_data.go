aa5265790df2f336cb347b8dee51724e7cdb1cbc
Roman Poletaev <losaped@gmail.com>
2018-11-28 21:40:00 +0300

    set pub to keys as bytes

--- packages/migration/keys_data.go
+++ packages/migration/keys_data.go
@@ -2,4 +2,4 @@ package migration
 
 import "github.com/GenesisKernel/go-genesis/packages/consts"
 
-var keysDataSQL = `INSERT INTO "1_keys" (id, pub, blocked, ecosystem) VALUES (` + consts.GuestKey + `, '', 1, '%[1]d');`
+var keysDataSQL = `INSERT INTO "1_keys" (id, pub, blocked, ecosystem) VALUES (` + consts.GuestKey + `, decode('` + consts.GuestPublic + `', 'hex'), 1, '%[1]d');`
--- packages/migration/obs/keys_data.go
+++ packages/migration/obs/keys_data.go
@@ -1,8 +1,10 @@
 package obs
 
-import "github.com/GenesisKernel/go-genesis/packages/consts"
+import (
+	"github.com/GenesisKernel/go-genesis/packages/consts"
+)
 
 var keysDataSQL = `
 INSERT INTO "1_keys" (id, pub, blocked, ecosystem) 
-VALUES (` + consts.GuestKey + `, '', 1, '%[1]d');
+VALUES (` + consts.GuestKey + `, decode('` + consts.GuestPublic + `', 'hex'), 1, '%[1]d');
 `
