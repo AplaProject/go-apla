4f99d63bec09212a0b0844ca5765b5afc458c8e6
Evgeny Lerner <dvork1ng.egaas@gmail.com>
2018-12-13 13:40:39 +0300

    two missed contracts added

--- packages/migration/updates/m122.go
+++ packages/migration/updates/m122.go
@@ -8,13 +8,13 @@ ALTER TABLE "1_ecosystems" ADD COLUMN "type_withdraw" bigint NOT NULL DEFAULT '0
 
 UPDATE "1_tables" SET permissions = '
 {	"insert": "ContractAccess(\"@1NewEcosystem\")",
-	"update": "ContractAccess(\"@1EditEcosystemName\",\"@1VotingDecisionCheck\",\"@1EcManageInfo\",\"@1TeCreate\",\"@1TeChange\")",
+	"update": "ContractAccess(\"@1EditEcosystemName\",\"@1VotingDecisionCheck\",\"@1EcManageInfo\",\"@1TeCreate\",\"@1TeChange\",\"@1TeBurn\")",
 	"new_column": "ContractConditions(\"@1AdminCondition\")"
 }', columns = '
 {	"name": "ContractAccess(\"@1EditEcosystemName\")",
 	"info": "ContractAccess(\"@1EcManageInfo\")",
 	"is_valued": "ContractAccess(\"@1VotingDecisionCheck\")",
-	"emission_amount": "ContractAccess(\"@1TeCreate\")",
+	"emission_amount": "ContractAccess(\"@1TeCreate\",\"@1TeBurn\")",
 	"token_title": "ContractAccess(\"@1TeCreate\")",
 	"type_emission": "ContractAccess(\"@1TeCreate\",\"@1TeChange\")",
 	"type_withdraw": "ContractAccess(\"@1TeCreate\",\"@1TeChange\")"
