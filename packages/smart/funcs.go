24176d413f13bc0fc0a088d86e3ca69dd3838972
Dmitriy Chertkov <dmitriychertkov@gmail.com>
2019-07-11 11:13:01 +0500

    Fixed stack check in the ColumnCondition function

--- packages/smart/funcs.go
+++ packages/smart/funcs.go
@@ -1339,7 +1339,7 @@ func ColumnCondition(sc *SmartContract, tableName, name, coltype, permissions st
 		return err
 	}
 
-	isExist := accessContracts(sc, nEditContract)
+	isExist := accessContracts(sc, nEditColumn)
 	tEx := &model.Table{}
 	prefix := converter.Int64ToStr(sc.TxSmart.EcosystemID)
 	tEx.SetTablePrefix(prefix)
