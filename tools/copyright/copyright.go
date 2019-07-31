a19259a53dcbd56fa5f0de56927cbe85ed4e84c6
Alexey Krivonogov <akrivonogov@gmail.com>
2019-07-31 14:56:36 +0500

    feature/1394 license (#739)
    * Fixed copyright
    
    * Removed empty files
    
    * Changed license
    
    * Fixed copyright tool
    
    * Fixed copyright tool
    
    * Fixed copyright tool
    
    * Changed headers

--- tools/copyright/copyright.go
+++ tools/copyright/copyright.go
@@ -1,3 +1,19 @@
+// Copyright (C) 2017, 2018, 2019 EGAAS S.A.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or (at
+// your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
 package main
 
 import (
@@ -11,6 +27,10 @@ import (
 
 // The program inserts copyright notice at the beginning of .go files.
 
+const (
+	codeGen = `// Code generated`
+)
+
 var (
 	copyright []byte
 )
@@ -21,7 +41,12 @@ func ProcessDir(dir string, recurse bool) {
 	if err != nil {
 		fmt.Println(err)
 	}
-
+	path, err := filepath.Abs(dir)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	path = path[strings.Index(path, `go-apla`)+7:]
 	for _, file := range files {
 		fname := file.Name()
 		fullName := filepath.Join(dir, fname)
@@ -31,12 +56,31 @@ func ProcessDir(dir string, recurse bool) {
 				fmt.Println(err)
 				os.Exit(1)
 			} else {
-				if len(fdata) <= len(copyright) || bytes.Compare(fdata[:len(copyright)], copyright) != 0 {
+				var prefix string
+				if strings.HasPrefix(string(fdata[:len(codeGen)]), codeGen) {
+					fmt.Println(`...Code generated`)
+					continue
+				}
+				if bytes.Equal(fdata[:4], []byte(`// +`)) {
+					lines := strings.Split(string(fdata[:256]), "\n")
+					for _, line := range lines {
+						if strings.HasPrefix(line, `// +`) {
+							prefix += line + "\r\n"
+						} else {
+							break
+						}
+					}
+				}
+				if len(prefix) > 0 {
+					prefix += "\r\n"
+				}
+				off := bytes.IndexByte(fdata, 0xa)
+				if len(fdata) <= len(copyright) || !bytes.Equal(fdata[off+1:off+1+len(copyright)], copyright) {
 					off := bytes.Index(fdata, []byte(`package`))
 					if off == -1 {
 						fmt.Println(`...package has not been found`)
 					} else {
-						out := copyright
+						out := append([]byte(prefix), copyright...)
 						out = append(out, fdata[off:]...)
 						if err := ioutil.WriteFile(fullName, out, 0644); err == nil {
 							fmt.Println(`...Overwrited`)
@@ -62,7 +106,9 @@ func main() {
 			copyright = append(copyright, 0xa)
 		}
 		ProcessDir("../..", false)
+		ProcessDir("../../cmd", true)
 		ProcessDir("../../packages", true)
+		ProcessDir("../../tools", true)
 	} else {
 		fmt.Println(err)
 	}
